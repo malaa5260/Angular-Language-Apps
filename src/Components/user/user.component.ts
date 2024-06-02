87a1aa288a17e810ed96f9ca0e1daec1a1e7a674
Mohamed Alaa <mohamed.alaa.osman@outlook.com>
2024-06-02 12:19:51 +0300

    update version

--- src/Components/user/user.component.ts
+++ src/Components/user/user.component.ts
@@ -1,4 +1,15 @@
-import {Component, computed, EventEmitter, input, Input, InputSignal, Output, Signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  InputSignal,
+  output,
+  Output,
+  OutputEmitterRef,
+  Signal
+} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -14,10 +25,11 @@ import {NgOptimizedImage} from "@angular/common";
 export class UserComponent {
   //@Input({required:true }) avatar: string='';
   //@Input({required: true}) name: string = '';
-  @Output() userSelected: EventEmitter<string> = new EventEmitter();
+  //@Output() userSelected: EventEmitter<string> = new EventEmitter();
   avatar: InputSignal<string> = input.required<string>();
   name: InputSignal<string> = input.required<string>();
   id: InputSignal<string> = input.required<string>();
+  userSelected:OutputEmitterRef<string> =output<string>();
   imagePath: Signal<string> = computed(() => {
     return 'assets/users/' + this.avatar();
   })
