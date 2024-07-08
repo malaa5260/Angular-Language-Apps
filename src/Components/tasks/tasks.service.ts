4b1d0f1758119544cb1b994513fb7e7d386b8312
Mohamed Alaa <mohamed.alaa.osman@outlook.com>
2024-07-08 20:26:41 +0300

    finish section 2_63

--- src/Components/tasks/tasks.service.ts
+++ src/Components/tasks/tasks.service.ts
@@ -9,8 +9,11 @@ export class TasksService {
   private tasks = dummyTasks;
 
   constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
   }
-
 
   getUserTasks(userId: string) {
     return this.tasks.filter(task => task.userId === userId);
@@ -23,10 +26,16 @@ export class TasksService {
       title: taskData.title,
       summary: taskData.summary,
       dueDate: taskData.date,
-    })
+    });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
